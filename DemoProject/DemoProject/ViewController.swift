a85c3d7b984050d9e33939daae6146f7727f9156
Muhammad Abubaker Majeed <abubakermajeed@gmail.com>
2019-04-24 01:39:27 +0500

    example Added

--- DemoProject/DemoProject/ViewController.swift
+++ DemoProject/DemoProject/ViewController.swift
@@ -20,19 +20,18 @@ class ViewController: UIViewController {
 
     override func viewDidAppear(_ animated: Bool) {
 
-        let wrapper : ApnsToFcmConverter = ApnsToFcmConverter.init(googleLegacyServerkey: "AIzaSyCOEHG-Tnsh2heA2Zs7150Bko8MD6HMpok", senderID: "614872028604")
-
+        let wrapper : ApnsToFcmConverter = ApnsToFcmConverter.init(googleLegacyServerkey: "googleLegacyServerkey", senderID: "SenderID")
 //        wrapper.convertAPNsToFCMAndRegistered(importApnsFromFileURL: nil , isDebug: false) { (url, success ) in
 //
 //        }
-        wrapper.convertAPNsToFCMAndRegisteredToWithTopicName(importApnsFromFileURL: nil, isDebug: true , topic: "IOS-AirFrayer") { (url , success) in
-           // debugPrint("\(url), \(success)")
+        wrapper.convertAPNsToFCMAndRegisteredToWithTopicName(importApnsFromFileURL: nil, isDebug: false , topic: "IOS-App") { (url , success) in
+            debugPrint("\(url), \(success)")
             let alertController = UIAlertController(title: "Message", message: "Task : \(success)", preferredStyle: .alert)
             self.present(alertController, animated: true, completion: nil)
         }
-        wrapper.registerdExsistingFCMTokenToTopicName(importApnsFromFileURL: nil, isDebug: true, topic: "IOS-AirFrayer") { (_, success ) in
-
-        }
+//        wrapper.registerdExsistingFCMTokenToTopicName(importApnsFromFileURL: nil, isDebug: true, topic: "IOS-AirFrayer") { (_, success ) in
+//
+//        }
 
     }
 
